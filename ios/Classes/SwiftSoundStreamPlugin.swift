39b11df3d14b55ae33f36b48392936c98180e63e
Alexander.Li <superpowerlee@gmail.com>
2021-09-29 20:31:18 +0800

    增加setVoiceProcessingEnabled(true)

--- ios/Classes/SwiftSoundStreamPlugin.swift
+++ ios/Classes/SwiftSoundStreamPlugin.swift
@@ -195,6 +195,7 @@ public class SwiftSoundStreamPlugin: NSObject, FlutterPlugin {
     private func resetEngineForRecord() {
         mAudioEngine.inputNode.removeTap(onBus: mRecordBus)
         let input = mAudioEngine.inputNode
+        try input.setVoiceProcessingEnabled(true)
         let inputFormat = input.outputFormat(forBus: mRecordBus)
         let converter = AVAudioConverter(from: inputFormat, to: mRecordFormat!)!
         let ratio: Float = Float(inputFormat.sampleRate)/Float(mRecordFormat.sampleRate)
